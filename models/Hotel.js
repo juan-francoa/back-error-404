4dcaa72a8f8db50a13d50e3369d3898c8a7424e6
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-14 15:47:43 -0500

    MT-02-22

---
+++ models/Hotel.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose")
+const schema = new mongoose.Schema(
+    {
+        name: {type: String, required: true },
+        photo: [{type: String, required: true}],
+        capacity: {type: Number, required: true },
+        cityId: {type: mongoose.Types.ObjectId, ref: "cities", required: true },
+        userId: {type: mongoose.Types.ObjectId, ref: "users", required: true }
+    }
+)
+const Hotel = mongoose.model("hotels", schema)
+module.exports = Hotel
