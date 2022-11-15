1ce8e2cfa8c5bd04be4e87755d85a48eddabbc09
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-14 19:22:40 -0500

    MT-B1-27

--- controllers/city.js
+++ controllers/city.js
@@ -78,6 +78,30 @@ const controller = {
             });
         }
     },
+    update: async (req, res) => {
+        let { id } = req.params;
+
+        try {
+            let city = await City.findOneAndUpdate({ _id: id }, req.body, { new: true });
+            if(city){
+                res.status(200).json({
+                    success: true,
+                    message: 'City updated',
+                    data: city,
+                });
+            }else{
+                res.status(404).json({
+                    success: false,
+                    message: 'City not found',
+                });
+            }
+        }catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    },
 }
 
 module.exports = controller
