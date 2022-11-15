fb12c1b5ef067f7b3dad160d003e4b3ea812553b
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-14 21:58:37 -0500

    MT-B2-35

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,10 +1,11 @@
 let router = require('express').Router();
 
 const { Router } = require('express');
-let {read, create, update} = require('../controllers/itinerary');
+let {read, create, update, destroyOne} = require('../controllers/itinerary');
 
 router.get('/', read);
 router.post('/', create);
 router.put("/:id", update);
+router.delete("/:id", destroyOne);
 
 module.exports = router;
