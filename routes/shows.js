02a7a2d62788b75d50c78fab8b4e988ee55eede7
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-14 22:06:12 -0500

    MT-B2-36

--- routes/shows.js
+++ routes/shows.js
@@ -1,10 +1,11 @@
 let router = require("express").Router()
 
-let {read, create, update} = require("../controllers/show")
+let {read, create, update, destroyOne} = require("../controllers/show")
 
 
 router.route("/").get(read);
 router.route("/").post(create);
 router.route("/:id").put(update);
+router.route("/:id").delete(destroyOne)
 
 module.exports = router;
