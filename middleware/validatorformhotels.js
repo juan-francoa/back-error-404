ffc89da6efad3211c5c0bd749be5f1663eca1e43
pilisalto <m.pilarsalto@gmail.com>
2022-11-21 22:13:44 -0300

    MT-02-40

---
+++ middleware/validatorformhotels.js
@@ -0,0 +1,15 @@
+
+const validatorform = (schema) => [
+    (req, res, next) =>{
+        const data = schema.validate(req.body, {abortEarly: false})
+        if(data.error){
+            return res.json({
+                success: false,
+                message: data.error.details.map(err => err.message)
+            })
+        }
+        next()
+    }
+]
+
+module.exports = validatorform
