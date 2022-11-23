3cff8a49f2213be585286183857bab68a4e00396
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-23 18:06:59 -0500

    correccion

--- routes/hotels.js
+++ routes/hotels.js
@@ -1,10 +1,10 @@
 let router = require("express").Router()
 const schema1 = require("../schemas/newHotels")
-const validatorform = require("../middleware/validatorformhotels")
+const validator = require("../middleware/validator")
 
 let {create, read, readOne, update, destroyOne} = require("../controllers/hotel")
 
-router.route("/").post(validatorform(schema1), create);
+router.route("/").post(validator(schema1), create);
 router.route("/").get(read)
 router.route("/:id").get(readOne)
 router.route("/:id").put(update)
--- schemas/newCities.js
+++ schemas/newCities.js
@@ -5,30 +5,30 @@ const schema = joi.object({
         .min(3)
         .max(50)
         .messages({
-            "any.required": "This field is required, please introduce it",
+            "any.required": "name is required, please introduce it",
             "string.empty": "You missed the field name",
-            "string.min": "You need three letteres minimum",
-            "string.max" : "You need three letteres maximiun"
+            "string.min": "You need three letteres minimum in name",
+            "string.max" : "You need three letteres maximiun in name"
         }),
     continent: joi.string().required()
         .messages({
-            "any.required": "This field is required, please introduce it",
+            "any.required": "continent is required, please introduce it",
             "string.empty": "You missed the field continent"
         }),
     photo: joi.string().required()
         .uri()
         .messages({
-            "any.required": "This field is required, please introduce it",
+            "any.required": "photo is required, please introduce it",
             "string.empty": "You missed the field photo"
         }),
     population: joi.number().required()
         .messages({
-            "any.required": "This field is required, please introduce it",
-            "number.empty": "You missed the field population"
+            "any.required": "population is required, please introduce it",
+            "string.empty": "You missed the field population"
         }),
     userId: joi.any().required()
         .messages({
-            "any.required": "This field is required, please introduce it",
+            "any.required": "userId is required, please introduce it",
             "string.empty": "You missed the field userId"
         })
 
