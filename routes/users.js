a3a4ccc0fc4fce471ba87ffdb92cc9ea4257e436
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-26 16:29:19 -0500

    validacion para login

--- routes/users.js
+++ routes/users.js
@@ -7,10 +7,11 @@ let {accountExistsSignIn} = require("../middleware/accountExistsSignIn")
 let {accountHasBeenVerified} = require("../middleware/accountHasBeenVerified")
 let mustSignIn = require("../middleware/mustSignIn")
 let passport = require("../config/passport")
+let schemaLogin = require("../schemas/userLogin")
 
 router.route("/sign-up").post( validator(schema),accountExists,registrar)
 router.get("/verify/:code",verificar)
-router.post("/sign-in" ,accountExistsSignIn, accountHasBeenVerified,ingresar)
+router.post("/sign-in" ,validator(schemaLogin),accountExistsSignIn, accountHasBeenVerified,ingresar)
 router.post("/token",passport.authenticate("jwt",{session:false}), mustSignIn, ingresarConToken )
-//passport.authenticate("jwt",{session:false})
+
 module.exports = router;
