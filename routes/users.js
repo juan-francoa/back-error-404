90a947afc61331077148020765cc37f6ce4b025b
pilisalto <m.pilarsalto@gmail.com>
2022-11-27 13:57:21 -0300

    MT-01-49

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 let router = require("express").Router()
 const validator = require("../middleware/validator")
 const schema = require("../schemas/user")
-let {registrar, verificar, ingresar, ingresarConToken} = require("../controllers/user")
+let {registrar, verificar, ingresar, ingresarConToken, salir} = require("../controllers/user")
 let {accountExists} = require("../middleware/accountExistsSignUp")
 let {accountExistsSignIn} = require("../middleware/accountExistsSignIn")
 let {accountHasBeenVerified} = require("../middleware/accountHasBeenVerified")
@@ -13,5 +13,6 @@ router.route("/sign-up").post( validator(schema),accountExists,registrar)
 router.get("/verify/:code",verificar)
 router.post("/sign-in" ,validator(schemaLogin),accountExistsSignIn, accountHasBeenVerified,ingresar)
 router.post("/token",passport.authenticate("jwt",{session:false}), mustSignIn, ingresarConToken )
+router.put("/sign-out", passport.authenticate("jwt",{session:false}), salir)
 
 module.exports = router;
