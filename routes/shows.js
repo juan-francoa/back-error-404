fb97d13d8fcb32c17ccc8aa6e82cb6e7625afe13
pilisalto <m.pilarsalto@gmail.com>
2022-11-28 03:08:57 -0300

    MT-02-52

--- routes/shows.js
+++ routes/shows.js
@@ -1,11 +1,13 @@
 let router = require("express").Router()
-
 let {read, create, update, destroyOne} = require("../controllers/show")
+const passport = require('../config/passport')
+const validator = require('../middlewares/validator');
+const schemaShows = require('../schemas/show');
 
 
-router.route("/").get(read);
-router.route("/").post(create);
-router.route("/:id").put(update);
-router.route("/:id").delete(destroyOne)
+router.get('/', read);
+router.post('/', passport.authenticate("jwt", { session: false }), validator(schemaShows), create);
+router.patch('/:id', passport.authenticate("jwt", { session: false }),  update);
+router.delete('/:id', passport.authenticate("jwt", { session: false }),  destroyOne);
 
 module.exports = router;
