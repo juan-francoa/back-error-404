bdfd2a19e0141b53b5705eb5e8f9d49f521d28a0
pilisalto <m.pilarsalto@gmail.com>
2022-11-28 03:04:02 -0300

    MT-01-51

--- routes/itineraries.js
+++ routes/itineraries.js
@@ -1,11 +1,13 @@
 let router = require('express').Router();
-
 const { Router } = require('express');
 let {read, create, update, destroyOne} = require('../controllers/itinerary');
+const passport = require('../config/passport')
+const validator = require('../middlewares/validator');
+const schemaItinerary = require('../schemas/itinerary');
 
 router.get('/', read);
-router.post('/', create);
-router.put("/:id", update);
-router.delete("/:id", destroyOne);
+router.post('/', passport.authenticate("jwt", { session: false }), validator(schemaItinerary), create);
+router.put('/:id', passport.authenticate("jwt", { session: false }), update);
+router.delete('/:id', passport.authenticate("jwt", { session: false }), destroyOne);
 
 module.exports = router;
