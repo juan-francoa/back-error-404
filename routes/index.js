cebcf333b61d8f870594088d806cbd0059247cbb
juan-francoa <esteban_rlt159@hotmail.com>
2022-11-29 16:04:38 -0500

    MT-02-55/03

--- routes/index.js
+++ routes/index.js
@@ -6,11 +6,13 @@ let cities = require("./cities")
 let itineraries = require("./itineraries")
 let hotels = require("./hotels")
 let shows = require("./shows")
+let reactions = require("./reactions")
 
 router.use("/auth", user)
 router.use("/cities", cities)
 router.use("/itineraries", itineraries)
 router.use("/hotels", hotels)
 router.use("/shows", shows)
+router.use("/reactions",reactions)
 
 module.exports = router;
