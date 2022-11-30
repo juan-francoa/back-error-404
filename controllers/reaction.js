996eedd1b83329a941d8ba2463b10fd1f49577ac
pilisalto <m.pilarsalto@gmail.com>
2022-11-30 12:56:20 -0300

    MT-01-55

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -4,6 +4,7 @@ const Reaction = require('../models/Reaction');
 const controller = {
 
     create: async (req, res) => {
+
         try{
             let reaction = await Reaction.create(req.body)
             if(reaction){
@@ -13,13 +14,42 @@ const controller = {
                 })
             }
         }
+
         catch(error){
             res.status(400).json({
                 success:true,
                 message: error.message
             })
         }
-    }
+    },
+
+    update: async (req, res) => {
+
+        let {itineraryId, name} = req.params;
+
+        try{
+            let oneReaction = await Reaction.findByIdAndUpdate({_id: itineraryId}, req.body, {new: true});
+            if (oneReaction) {
+                res.status(200).json({
+                    success: true,
+                    message: 'Reaction posted saved',
+                    data: oneReaction,
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'Reaction posted not saved',
+                });
+            }
+        }
+
+        catch (error) {
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+},
 
 }
 
