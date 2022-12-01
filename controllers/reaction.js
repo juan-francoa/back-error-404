e067b4bc2031724fc855d040776effcd7f18eac9
Pilar Salto <m.pilarsalto@gmail.com>
2022-12-01 13:53:40 -0300

    MT-01-56/01

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -94,7 +94,6 @@ const controller = {
                     message: 'Reactions not length',
                 });
             }
-
         }
         catch(error){
             res.status(400).json({
@@ -102,7 +101,34 @@ const controller = {
                 message: error.message,
             });
         }
-    }    
+    },
+    
+    reactionsUser: async (req, res) => {
+        let {query} = req;
+        try {
+            let reactionsUser = await Reaction.find ()
+            reactionsUser = reactionsUser.filter(e=>  e.userId.includes(query.userId))
+            if (reactionsUser) {
+                res.status(200).json({
+                    success: true,
+                    message: 'Reactions for user',
+                    data: reactionsUser,
+                });
+            } else {
+                res.status(404).json({
+                    success: false,
+                    message: 'Not reactions for user',
+                });
+            }
+        }
+        catch(error){
+            res.status(400).json({
+                success: false,
+                message: error.message,
+            });
+        }
+    }
 }
+
 
 module.exports = controller;
--- routes/reactions.js
+++ routes/reactions.js
@@ -1,11 +1,12 @@
 let router = require("express").Router()
 const passport = require('../config/passport')
 const validator = require('../middleware/validator');
-const {create,update,cantidadReactions} =  require("../controllers/reaction")
+const {create,update,cantidadReactions,reactionsUser} =  require("../controllers/reaction")
 const schemaReaction = require("../schemas/reaction")
 
 router.post("/", validator(schemaReaction), create)
-router.put("/",passport.authenticate("jwt", { session: false }), update)
+router.put("/", passport.authenticate("jwt", { session: false }), update)
 router.get("/", cantidadReactions)
+router.get("/?", passport.authenticate("jwt", { session: false }), reactionsUser)
 
 module.exports = router;
