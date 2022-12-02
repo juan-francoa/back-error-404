cd5ebaf6bf9d53fbd791ffdeb158cfa00dbf4dbd
juan-francoa <esteban_rlt159@hotmail.com>
2022-12-01 19:17:54 -0500

    MT-01-56

--- controllers/reaction.js
+++ controllers/reaction.js
@@ -5,19 +5,19 @@ const controller = {
 
     create: async (req, res) => {
 
-        try{
+        try {
             let reaction = await Reaction.create(req.body)
-            if(reaction){
+            if (reaction) {
                 res.status(201).json({
-                    success:true,
-                    message:"reaction create"
+                    success: true,
+                    message: "reaction create"
                 })
             }
         }
 
-        catch(error){
+        catch (error) {
             res.status(400).json({
-                success:true,
+                success: true,
                 message: error.message
             })
         }
@@ -25,37 +25,37 @@ const controller = {
 
     update: async (req, res) => {
 
-        let {query} = req;
+        let { query } = req;
         let userId = req.user._id
-        try{
+        try {
             let itinerary = await Reaction.find()
             let busq = {}
-            for(let name in query){
-                if( name === "name"){
-                    if(query.name === "like"){
-                        itinerary = itinerary.filter(e=> e.icon == "https://img.icons8.com/material-outlined/2x/facebook-like--v2.png")
+            for (let name in query) {
+                if (name === "name") {
+                    if (query.name === "like") {
+                        itinerary = itinerary.filter(e => e.icon == "https://img.icons8.com/material-outlined/2x/facebook-like--v2.png")
                     }
-                    else if(query.name === "not-like"){
-                        itinerary = itinerary.filter(e=> e.icon == "https://img.icons8.com/material-outlined/2x/thumbs-down.png")
+                    else if (query.name === "not-like") {
+                        itinerary = itinerary.filter(e => e.icon == "https://img.icons8.com/material-outlined/2x/thumbs-down.png")
                     }
-                    else if(query.name === "love"){
-                        itinerary = itinerary.filter(e=> e.icon == "https://img.icons8.com/material-outlined/512/hearts.png")
+                    else if (query.name === "love") {
+                        itinerary = itinerary.filter(e => e.icon == "https://img.icons8.com/material-outlined/512/hearts.png")
                     }
-                    else if(query.name === "surprise"){
-                        itinerary = itinerary.filter(e=> e.icon == "https://img.icons8.com/material-outlined/2x/surprised.png")
+                    else if (query.name === "surprise") {
+                        itinerary = itinerary.filter(e => e.icon == "https://img.icons8.com/material-outlined/2x/surprised.png")
                     }
                 }
-                if( name === "itineraryId"){
-                    itinerary = itinerary.filter(e=>  e.itineraryId == query.itineraryId)
+                if (name === "itineraryId") {
+                    itinerary = itinerary.filter(e => e.itineraryId == query.itineraryId)
                 }
             }
-            if(!itinerary[0].userId.includes(userId)){
-                busq = {$push:{userId:userId}}
+            if (!itinerary[0].userId.includes(userId)) {
+                busq = { $push: { userId: userId } }
             }
-            else{
-                busq = {$pull:{userId:userId}}
+            else {
+                busq = { $pull: { userId: userId } }
             }
-            let oneReaction = await Reaction.findByIdAndUpdate({_id: itinerary[0]._id}, busq, {new: true});
+            let oneReaction = await Reaction.findByIdAndUpdate({ _id: itinerary[0]._id }, busq, { new: true });
             if (oneReaction) {
                 res.status(200).json({
                     success: true,
@@ -69,65 +69,86 @@ const controller = {
                 });
             }
         }
-        catch(error){
-            res.status(400).json({
-                success: false,
-                message: error.message,
-            });
-        }
-},
-
-    cantidadReactions: async (req, res) => {
-        let {query} = req;
-        try {
-            let reactions = await Reaction.find ()
-            reactions = reactions.filter(e=>  e.itineraryId == query.itineraryId)
-            if (reactions) {
-                res.status(200).json({
-                    success: true,
-                    message: 'Reactions length',
-                    data: reactions,
-                });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: 'Reactions not length',
-                });
-            }
-        }
-        catch(error){
+        catch (error) {
             res.status(400).json({
                 success: false,
                 message: error.message,
             });
         }
     },
-    
-    reactionsUser: async (req, res) => {
-        let {query} = req;
+
+    read: async (req, res) => {
+        let { query } = req;
         try {
-            let reactionsUser = await Reaction.find ()
-            reactionsUser = reactionsUser.filter(e=>  e.userId.includes(query.userId))
-            if (reactionsUser) {
-                res.status(200).json({
-                    success: true,
-                    message: 'Reactions for user',
-                    data: reactionsUser,
-                });
-            } else {
-                res.status(404).json({
-                    success: false,
-                    message: 'Not reactions for user',
-                });
+            if (query.itineraryId) {
+                let reactions = await Reaction.find()
+                reactions = reactions.filter(e => e.itineraryId == query.itineraryId)
+                if (reactions) {
+                    res.status(200).json({
+                        success: true,
+                        message: 'Reactions length',
+                        data: reactions,
+                    });
+                } else {
+                    res.status(404).json({
+                        success: false,
+                        message: 'Reactions not length',
+                    });
+                }
             }
+            else if (query.userId) {
+                let reactionsUser = await Reaction.find().populate({ path: 'itineraryId', select: 'name photo _id' })
+    
+                reactionsUser = reactionsUser.filter(e => e.userId.includes(query.userId))
+                if (reactionsUser) {
+                    res.status(200).json({
+                        success: true,
+                        message: 'Reactions for user',
+                        data: reactionsUser,
+                    });
+                } else {
+                    res.status(404).json({
+                        success: false,
+                        message: 'Not reactions for user',
+                    });
+                }
+            }
+
         }
-        catch(error){
+        catch (error) {
             res.status(400).json({
                 success: false,
                 message: error.message,
             });
         }
-    }
+    },
+    deleteReaction: async (req, res) => {
+        let { id } = req.params
+
+        try {
+            let reaction = await Reaction.findOneAndUpdate({ _id: id }, { $pull: { userId: req.user._id } }, { new: true })
+            .populate({ path: 'itineraryId', select: 'name photo _id' })
+            if (reaction) {
+                res.status(200).json({
+                    data: reaction,
+                    message: `reaction deleted`,
+                    success: true,
+                })
+            } else {
+                res.status(404).json({
+                    message: `reaction not found`,
+                    success: false,
+                })
+            }
+        } catch (error) {
+            res.status(400).json({
+                message: error.message,
+                success: false
+            })
+        }
+    },
+
+
 }
 
 
--- models/Reaction.js
+++ models/Reaction.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const schema = new mongoose.Schema({
-    itineraryId: {type: mongoose.Types.ObjectId, ref: "itinerary", required: true },
+    itineraryId: {type: mongoose.Types.ObjectId, ref: "itineraries", required: true },
     name:{type: String , required: true},
     icon: {type: String, required: true},
     iconBack: {type: String, required: true},
